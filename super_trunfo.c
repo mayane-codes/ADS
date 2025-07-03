29e98376b9c012676b3049904d886c9b47c77c54
mayane-codes <mayane.thayna14@hotmail.com>
2025-07-03 19:27:53 -0300

    Update super_trunfo.c, inserindo carta 2

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,6 +1,6 @@
 /*
 autor: Mayane Thainá
-data: 01/07/2025
+data: 03/07/2025
 descrição: desenvolvimento de cartas para o jogo super trunfo
 
 
@@ -8,44 +8,112 @@ descrição: desenvolvimento de cartas para o jogo super trunfo
 #include <stdio.h>
 
 int main(){
-char estado;
-char codigo_carta[3];
-char cidade[50];
-int populacao;
-float area_territorio;
-float pib;
-int ponto_turistico;
+char estado_carta1;
+char estado_carta2;
+char codigo_carta1[3]; 
+char codigo_carta2[3];
+char cidade_carta1[50]; 
+char cidade_carta2[50];
+int populacao_carta1;
+int populacao_carta2;
+float area_territorio_carta01; 
+int area_territorio_carta2;
+float pib_carta1;
+float pib_carta2;
+int ponto_turistico_carta1; 
+int ponto_turistico_carta2;
+
+printf("\nSolicitando dados da carta1\n");
 
 printf("seu estado: ");
-scanf("%c",  &estado);
+scanf("%c",  &estado_carta1);
+while (getchar() != '\n');
 
 printf("codigo da carta: ");
-scanf("%s", codigo_carta);
+scanf("%s", codigo_carta1);
+while (getchar() != '\n');
 
 printf("sua cidade: ");
-scanf("%s", cidade);
+scanf("%s", cidade_carta1);
+while (getchar() != '\n');
+
 
 printf("digite o numero de habitantes: ");
-scanf("%d", &populacao);
+scanf("%d", &populacao_carta1);
+while (getchar() != '\n');
+
 
 printf("área em km2: ");
-scanf("%f", &area_territorio);
+scanf("%f", &area_territorio_carta01);
+while (getchar() != '\n');
+
 
 printf("seu PIB: ");
-scanf("%f", &pib);
+scanf("%f", &pib_carta1);
+while (getchar() != '\n');
+
 
 printf("pontos turisticos: ");
-scanf("%d", &ponto_turistico);
+scanf("%d", &ponto_turistico_carta1);
+while (getchar() != '\n');
 
-printf("Carta: 1\n");
-printf("\nEstado: %c", estado);
-printf("\nCódigo: %s", codigo_carta);
-printf("\nNome da cidade: %s", cidade);
-printf("\nPopulação: %d", populacao);
-printf("\nÁrea em km2: %f", area_territorio);
-printf("\nPIB: %f", pib);
-printf("\nPontos turisticos: %d", ponto_turistico);
+printf("\nsolicitando dados da carta2\n");
+
+printf("seu estado: ");
+scanf("%c",  &estado_carta2);
+while (getchar() != '\n');
+
+
+printf("codigo da carta: ");
+scanf("%s", codigo_carta2);
+while (getchar() != '\n');
+
+
+printf("sua cidade: ");
+scanf("%s", cidade_carta2);
+while (getchar() != '\n');
+
+
+printf("digite o numero de habitantes: ");
+scanf("%d", &populacao_carta2);
+while (getchar() != '\n');
+
+
+printf("área em km2: ");
+scanf("%f", &area_territorio_carta2);
+while (getchar() != '\n');
+
+
+printf("seu PIB: ");
+scanf("%f", &pib_carta2);
+while (getchar() != '\n');
+
+
+printf("pontos turisticos: ");
+scanf("%d", &ponto_turistico_carta2);
+while (getchar() != '\n');
+
+
+printf("\nCarta: 1\n");
+printf("\nEstado: %c", estado_carta1);
+printf("\nCódigo: %s", codigo_carta1);
+printf("\nNome da cidade: %s", cidade_carta1);
+printf("\nPopulação: %d", populacao_carta1);
+printf("\nÁrea em km2: %f", area_territorio_carta01);
+printf("\nPIB: %f", pib_carta1);
+printf("\nPontos turisticos: %d", ponto_turistico_carta1);
 printf("\n");
+
+printf("\nCarta: 2\n");
+printf("\nEstado: %c", estado_carta2);
+printf("\nCódigo: %s", codigo_carta2);
+printf("\nNome da cidade: %s", cidade_carta2);
+printf("\nPopulação: %d", populacao_carta2);
+printf("\nÁrea em km2: %f", area_territorio_carta2);
+printf("\nPIB: %f", pib_carta2);
+printf("\nPontos turisticos: %d", ponto_turistico_carta2);
+printf("\n");
+
 
 return 0;
  
